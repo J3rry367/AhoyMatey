ac5ad94f476d8b0a2f8221df64e3d48ddc227bbb
Jerry <jerry.johnson367@gmail.com>
2018-03-02 09:13:19 -0800

    added multiplayer cameras imported Pirate ship

--- Assets/Players/Player.cs
+++ Assets/Players/Player.cs
@@ -9,12 +9,6 @@ public class Player : NetworkBehaviour {
 
     private Vector3 inputValue;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
         if (!isLocalPlayer)
         {
@@ -27,4 +21,10 @@ public class Player : NetworkBehaviour {
 
         transform.Translate(inputValue);
 	}
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponentInChildren<Camera>().enabled = true;
+        GetComponentInChildren<AudioListener>().enabled = true;
+    }
 }
