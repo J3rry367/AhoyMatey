55f0d0661571e941242ffe33b3f5d353056f6ba8
Jerry <jerry.johnson367@gmail.com>
2018-03-02 06:41:17 -0800

    Started adding Custom Networking HUD

---
+++ Assets/MyHUD.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyHUD : MonoBehaviour {
+
+    private NetworkManager networkManager;
+    
+
+
+	// Use this for initialization
+	void Start () {
+        networkManager = GetComponent<NetworkManager>();
+	}
+	
+    public void MyStartHost()
+    {
+        Debug.Log("Starting host at " + Time.timeSinceLevelLoad);
+        networkManager.StartHost();
+    }
+
+    void OnStartHost()
+    {
+        Debug.Log("Host started at " + Time.timeSinceLevelLoad);
+    }
+}
